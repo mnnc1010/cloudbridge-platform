294b05cc170ab2c25a62efc40242ace92c0f91b4
Narendra Manna <mnnc1010@gmail.com>
2025-04-04 00:07:08 -0400

    Update angular module

--- frontend/angular-summary/src/app/models/resource.ts
+++ frontend/angular-summary/src/app/models/resource.ts
@@ -1,16 +1,19 @@
-/**
- * Interface representing a resource item.
- *
- * @property fileName - The name of the file.
- * @property fileType - The type of the file.
- * @property fileStorage - The storage location (e.g., "Mongo", "Dynamo", "S3").
- * @property dateInserted - The ISO date string when the file was inserted.
- * @property dateModified - The ISO date string when the file was last modified.
- */
-export interface Resource {
-  fileName: string;
-  fileType: string;
-  fileStorage: string;
-  dateInserted: string;
-  dateModified: string;
-}
+ /**
+  * Interface representing a resource item.
+  *@property id - the unique generated id of file when uploaded.
+  * @property fileName - The name of the file.
+  * @property fileType - The type of the file.
+  * @property fileDescription - the description if file.
+  * @property fileStorage - The storage location (e.g., "Mongo", "Dynamo", "S3").
+  * @property dateInserted - The ISO date string when the file was inserted.
+  * @property dateModified - The ISO date string when the file was last modified.
+  */
+  export interface Resource {
+      id: string;
+      fineName: string;
+      fileType: string;
+      fileDescription: string;
+      fileStorage: string;
+      dateInserted: string;
+      dateModified: string;
+      }
